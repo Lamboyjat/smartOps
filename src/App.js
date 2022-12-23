0d9945c1e4dd065779ca45a2f0d6132b31997903
lamboyjat1 <lamboyjat@gmail.com>
2022-12-23 17:38:42 +0200

    modify the app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import './App.css';
+import Home from './components/Home/Home';
+import Ships from './components/Ships/Ships'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Ships />
+      <Home />
     </div>
   );
 }
